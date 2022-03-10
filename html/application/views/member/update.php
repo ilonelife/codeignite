aec548fac62d53331424678a9c8ea0f7f6f663d0
iloneslife <jasony1@naver.com>
2022-03-10 16:08:53 +0900

    update 강의판

--- html/application/views/member/update.php
+++ html/application/views/member/update.php
@@ -1,6 +1,6 @@
 회원정보 수정
 <form method="post" action="/index.php/member/modity">
-<input type="hidden" name="id" value="<?php echo $result->_id?>"><br/>
+<input type="hidden" name="id" value="<?php echo $email?>"><br/>
     이메일 : <input type="text" name="email" value="<?php echo $result->email?>"><br />
     비밀번호 : <input type="password"  name="old_password" value=""> <br />
     새비밀번호 : <input type="password" name="new_password" value=""> <br />
